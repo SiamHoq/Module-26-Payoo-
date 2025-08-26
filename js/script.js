396d104e0a8443660852b635bd55883e20c16523
Siam Haque <siamhoq@gmail.com>
2025-08-26 19:02:04 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//log in button functionality
+// console.log('log in button clicked')
+// document.getElementById('login-Btn').addEventListener('click',function(){
+//     console.log('log in button clicked')
+// })
+
+document.getElementById('loginBtn').addEventListener('click',function(){
+    console.log('log in button clicked')
+})
